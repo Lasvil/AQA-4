5e8fdfa21aa7060ee85f3df17b7a95cedf47bd05
ArtyomKiyashko <lounershoargent@gmail.com>
2023-10-24 21:03:46 +0300

    final my work today

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/w0ihqg80c3oft3l7/branch/main?svg=true)](https://ci.appveyor.com/project/Lasvil/aqa-4/branch/main)
--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,10 +1,37 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class CardDeliveryTest {
 
+
     @Test
-    public void ShouldHappyTest(){
-
-
+    public void ShouldHappyTest() throws InterruptedException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.plusDays(3);
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form_size_m");
+        form.$("[placeholder='Город']").setValue("Архангельск");
+        form.$("[placeholder='Дата встречи']").setValue(String.valueOf(localDate));
+        form.$("[name='name']").setValue("Иван Царь");
+        form.$("[name='phone']").setValue("+79286664455");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button_view_extra").click();
+        SelenideElement form2 = $(".notification_visible").should(visible, Duration.ofSeconds(15));
+        form2.$(".notification__title").shouldHave(exactText("Успешно!"));
+        Thread.sleep(900);
     }
 }
