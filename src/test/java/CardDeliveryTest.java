8011e03b7b48d242677bcc41ea5e53d9e21c2867
ArtyomKiyashko <lounershoargent@gmail.com>
2023-10-25 19:20:58 +0300

    final my work today 2

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,10 +1,11 @@
-import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
 
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -18,20 +19,22 @@ public class CardDeliveryTest {
 
 
     @Test
-    public void ShouldHappyTest() throws InterruptedException {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate localDate = LocalDate.now();
-        localDate = localDate.plusDays(3);
+    public void shouldHappyTest(){
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(3);
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int dayOfMonth = date.getDayOfMonth();
         open("http://localhost:9999/");
         SelenideElement form = $(".form_size_m");
         form.$("[placeholder='Город']").setValue("Архангельск");
-        form.$("[placeholder='Дата встречи']").setValue(String.valueOf(localDate));
+        form.$("[placeholder='Дата встречи']").setValue(String.valueOf(date));
         form.$("[name='name']").setValue("Иван Царь");
         form.$("[name='phone']").setValue("+79286664455");
         form.$("[data-test-id='agreement']").click();
         form.$(".button_view_extra").click();
         SelenideElement form2 = $(".notification_visible").should(visible, Duration.ofSeconds(15));
-        form2.$(".notification__title").shouldHave(exactText("Успешно!"));
-        Thread.sleep(900);
+        form2.$(".notification__title").shouldHave(exactText("Успешно!")).should(visible);
+        form2.$(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dayOfMonth + "." + month + "." + year));
     }
 }
